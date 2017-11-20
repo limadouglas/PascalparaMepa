1e2481e08a0d825928ebb978e14f7252781d69a5
Douglas Henrique <lima.dougglas@gmail.com>
2017-11-20 12:06:46 -0200

    inicio

---
+++ src/Conversor.java
@@ -0,0 +1,22 @@
+import java.io.*;
+
+public class Conversor {
+
+    void gerarTokens(File arquivo) throws FileNotFoundException {
+        Reader reader = new FileReader(arquivo);
+
+
+        try{
+
+            FileReader arq = new FileReader(arquivo);
+            BufferedReader lerArq = new BufferedReader(arq);
+
+        }catch(Exception e){
+
+        }
+
+
+
+    }
+
+}
