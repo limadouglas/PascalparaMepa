74cb78c8cd1808a61a0dd7778fabcc9336e338b5
Douglas Henrique <sevsolucoes@gmail.com>
2017-11-28 20:14:17 -0200

    trabalho finalizado!

--- src/Principal.java
+++ src/Principal.java
@@ -1,6 +1,20 @@
 // Douglas Henrique --- 551066
 // Matheus Ferreira --- 547980
 
+/* OBSERVAÇÕES
+
+    O script em pascal tem que ter todos os tokens separados por um espaço,
+    exemplo correto  : a := 1 + 1 ;
+    exemplo incorreto: a:=1+1;
+
+    As ultimas linhas deste arquivo possuem dois codigos em pascal testados neste exemplo.
+
+    todos os exercicios do trabalho foram implementados!
+
+    o codigo mepa é mostrado no console e não em um arquivo separado!
+
+ */
+
 import java.io.File;
 import java.io.FileNotFoundException;
 
